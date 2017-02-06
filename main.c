5578cd481e646f0e3692bf2ff1397bdfc68c3437
Mike Mallin <mike@mremallin.ca>
2017-02-06 17:58:22 -0500

    Added Main Event Loop
    - Able to quit

--- main.c
+++ main.c
@@ -1,8 +1,24 @@
 #include <SDL.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
+
+void
+main_event_loop (void)
+{
+    SDL_Event e;
+    bool quit = false;
+
+    while (!quit) {
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+    }
+}
 
 int
 main (int argc, char *argv[])
@@ -14,22 +30,22 @@ main (int argc, char *argv[])
         printf("SDL could not initialize! SDL_Error: %s\n",
                SDL_GetError());
         return -1;
-    } else {
-        window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED,
-                                  SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
-                                  SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-        if (window == NULL) {
-            printf("Window could not be created! SDL_Error: %s\n",
-                   SDL_GetError());
-            return -1;
-        } else {
-            screenSurface = SDL_GetWindowSurface(window);
-            SDL_FillRect(screenSurface, NULL,
-                         SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-            SDL_UpdateWindowSurface(window);
-            SDL_Delay(2000);
-        }
     }
+
+    window = SDL_CreateWindow("Artillery Range", SDL_WINDOWPOS_UNDEFINED,
+                              SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+                              SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (window == NULL) {
+        printf("Window could not be created! SDL_Error: %s\n",
+               SDL_GetError());
+        return -1;
+    }
+
+    screenSurface = SDL_GetWindowSurface(window);
+    SDL_FillRect(screenSurface, NULL,
+                 SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+    SDL_UpdateWindowSurface(window);
+    main_event_loop();
 
     SDL_DestroyWindow(window);
     SDL_Quit();
